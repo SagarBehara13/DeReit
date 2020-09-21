20692e4335a659850bb803b5a690ea78dd6c758f
Sagar Behara <sagar@cryptocontrol.io>
2020-09-22 01:24:45 +0530

    updated remix code to fetch price,rent,surfaceArea

--- remix/RealEstateAPIConsumer.sol
+++ remix/RealEstateAPIConsumer.sol
@@ -10,6 +10,7 @@ contract RealEstateAPIConsumer is ChainlinkClient {
     address private oracle;
     bytes32 private jobId;
     uint256 private fee;
+    uint public rent;
 
     constructor() public {
         setPublicChainlinkToken();
@@ -18,8 +19,13 @@ contract RealEstateAPIConsumer is ChainlinkClient {
         fee = 0.1 * 10 ** 18; // 0.1 LINK
     }
 
+    function realEstateData(string memory _name) public {
+        realestateArea(_name);
+        realestatePrice(_name);
+        realestateRent(_name);
+    }
 
-    function realestaeArea(string memory _name) public {
+    function realestateArea(string memory _name) private {
         Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
         request.add("name", _name);
         request.add("copyPath", "surfaceArea");
@@ -28,5 +34,27 @@ contract RealEstateAPIConsumer is ChainlinkClient {
 
     function fulfill(bytes32 _requestId, uint256 _surfaceArea) public recordChainlinkFulfillment(_requestId){
         surfaceArea = _surfaceArea;
+    }
+
+    function realestatePrice(string memory _name) private {
+        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfillPrice.selector);
+        request.add("name", _name);
+        request.add("copyPath", "closePrice");
+        sendChainlinkRequestTo(oracle, request, fee);
+    }
+
+    function fulfillPrice(bytes32 _requestId, uint256 _price) public recordChainlinkFulfillment(_requestId){
+        price = _price;
+    }
+
+    function realestateRent(string memory _name) private {
+        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfillRent.selector);
+        request.add("name", _name);
+        request.add("copyPath", "expectedRent");
+        sendChainlinkRequestTo(oracle, request, fee);
+    }
+
+    function fulfillRent(bytes32 _requestId, uint256 _rent) public recordChainlinkFulfillment(_requestId){
+        rent = _rent;
     }
 }
