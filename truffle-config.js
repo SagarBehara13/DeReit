1f24bc48689f134f74c8dc11a3f0b0cf80fe3274
Sagar Behara <sagar@cryptocontrol.io>
2020-09-22 02:42:17 +0530

    updated truffle config to deploy on kovan

--- truffle-config.js
+++ truffle-config.js
@@ -1,28 +1,9 @@
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * truffleframework.com/docs/advanced/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura accounts
- * are available for free at: infura.io/register.
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
- */
-
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+require('dotenv').config()
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const kovanUrl = process.env.KOVAN_URL;
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+console.log(mnemonic, kovanUrl);
 
 module.exports = {
   networks: {
@@ -30,6 +11,12 @@ module.exports = {
       host: "localhost",
       port: 7545,
       network_id: "5777"
+    },
+    kovan: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, kovanUrl);
+      },
+      network_id: 42
     }
   },
   mocha: {
