d27e1d0c48d4b84a24f9f5086b9b9958b7075f2f
Sagar Behara <sagar@cryptocontrol.io>
2020-09-26 01:42:03 +0530

    push before merge

--- contracts/RealEstateAPIConsumer.sol
+++ contracts/RealEstateAPIConsumer.sol
@@ -3,13 +3,19 @@ pragma solidity >=0.6.0 <0.7.0;
 import "@chainlink/contracts/src/v0.6/ChainlinkClient.sol";
 
 contract RealEstateAPIConsumer is ChainlinkClient {
-
-  uint256 public price;
-  uint public surfaceArea;
   address private oracle;
   bytes32 private jobId;
   uint256 private fee;
+  uint256 public price;
+  uint public surfaceArea;
   uint public rent;
+  bytes32 public latestRequestId;
+  bytes32 public tokenHash;
+
+  mapping(bytes32 => bytes32) internal receipts;
+  mapping(bytes32 => uint256) private currentRealEstatePrice;
+  mapping(bytes32 => uint256) private currentRealEstateRent;
+  mapping(bytes32 => uint256) private currentRealEstateArea;
 
   constructor() public {
     setPublicChainlinkToken();
@@ -24,7 +30,26 @@ contract RealEstateAPIConsumer is ChainlinkClient {
     realestateRent(_name);
   }
 
-  function realestateArea(string memory _name) public {
+  function returnClosePrice(string memory _name) public {
+    Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.myCallback.selector);
+    request.add("name", _name);
+    request.add("copyPath", "closePrice");
+    receipts[sendChainlinkRequestTo(oracle, request, fee)] = stringToBytes32(_name);
+  }
+
+  function myCallback(bytes32 _requestId, uint256 _price) public recordChainlinkFulfillment(_requestId){
+    tokenHash = receipts[_requestId];
+    delete receipts[_requestId];
+    currentRealEstatePrice[tokenHash] = _price;
+    latestRequestId = _requestId;
+    price = _price;
+  }
+
+  function getCurrentPriceByAddress(string memory _name) public view returns (uint256) {
+    return currentRealEstatePrice[stringToBytes32(_name)];
+  }
+
+  function realestateArea(string memory _name) private {
     Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
     request.add("name", _name);
     request.add("copyPath", "surfaceArea");
@@ -35,7 +60,7 @@ contract RealEstateAPIConsumer is ChainlinkClient {
     surfaceArea = _surfaceArea;
   }
 
-  function realestatePrice(string memory _name) private {
+  function realestatePrice(string memory _name) public returns (uint){
     Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfillPrice.selector);
     request.add("name", _name);
     request.add("copyPath", "closePrice");
@@ -55,5 +80,23 @@ contract RealEstateAPIConsumer is ChainlinkClient {
 
   function fulfillRent(bytes32 _requestId, uint256 _rent) public recordChainlinkFulfillment(_requestId){
     rent = _rent;
+  }
+
+  function stringToBytes32(string memory source) internal pure returns (bytes32 result) {
+    bytes memory tempEmptyStringTest = bytes(source);
+    if (tempEmptyStringTest.length == 0) {
+      return 0x0;
+    }
+    assembly {
+      result := mload(add(source, 32))
+    }
+  }
+
+  function bytes32ToStr(bytes32 _bytes32) private pure returns (string memory) {
+    bytes memory bytesArray = new bytes(32);
+    for (uint256 i; i < 32; i++) {
+      bytesArray[i] = _bytes32[i];
+    }
+    return string(bytesArray);
   }
 }
