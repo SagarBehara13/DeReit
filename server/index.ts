0534f09d0b1bd4cc3673e4c2b97900bae5b0c51e
Sagar Behara <sagar@cryptocontrol.io>
2020-09-25 19:00:32 +0530

    implemented cors

--- server/index.ts
+++ server/index.ts
@@ -8,10 +8,12 @@ import { open } from './database'
 import routes from './routes'
 
 const app = express()
+const cors = require('cors')
 console.log("Server is starting")
 
 const port = process.env.PORT || 5000
 
+app.use(cors())
 app.use(bodyParser.json({ limit: '100mb' }))
 app.use(bodyParser.urlencoded({ extended: true, limit: '100mb' }))
 app.use(routes)
