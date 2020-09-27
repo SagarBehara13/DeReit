52ba57f36a133fd71b00241a20932017baace58d
Sagar Behara <sagar@cryptocontrol.io>
2020-09-27 12:15:51 +0530

    updated remix codes

---
+++ remix/RealEstateAPIConsumer.sol
@@ -0,0 +1,116 @@
+pragma solidity >=0.6.0 <0.7.0;
+
+import "https://github.com/smartcontractkit/chainlink/evm-contracts/src/v0.6/ChainlinkClient.sol";
+
+
+contract RealEstateAPIConsumer is ChainlinkClient {
+
+    address private oracle;
+    bytes32 private jobId;
+    uint256 private fee;
+    uint256 public price;
+    uint public surfaceArea;
+    uint public rent;
+    bytes32 public latestRequestId;
+    bytes32 public tokenHash;
+
+    mapping(bytes32 => bytes32) internal receiptPrice;
+    mapping(bytes32 => bytes32) internal receiptArea;
+    mapping(bytes32 => bytes32) internal receiptRent;
+    mapping(bytes32 => uint256) private currentRealEstatePrice;
+    mapping(bytes32 => uint256) private currentRealEstateRent;
+    mapping(bytes32 => uint256) private currentRealEstateArea;
+
+    constructor() public {
+        setPublicChainlinkToken();
+        oracle = 0xAA1DC356dc4B18f30C347798FD5379F3D77ABC5b;
+        jobId = "c76fb336346f4a5591c90ca8242091d5";
+        fee = 0.1 * 10 ** 18; // 0.1 LINK
+    }
+
+    function realEstateData(string memory _name) public {
+        realestateArea(_name);
+        returnClosePrice(_name);
+        realestateRent(_name);
+    }
+
+    function returnClosePrice(string memory _name) private {
+        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.myCallback.selector);
+        request.add("name", _name);
+        request.add("copyPath", "closePrice");
+        receiptPrice[sendChainlinkRequestTo(oracle, request, fee)] = stringToBytes32(_name);
+    }
+
+    function myCallback(bytes32 _requestId, uint256 _price) public recordChainlinkFulfillment(_requestId){
+        tokenHash = receiptPrice[_requestId];
+        delete receiptPrice[_requestId];
+        currentRealEstatePrice[tokenHash] = _price;
+        latestRequestId = _requestId;
+        price = _price;
+    }
+
+    function test() public returns(string memory){
+        return "hello";
+    }
+
+    function getCurrentPriceByAddress(string memory _name) public view returns (uint256) {
+        return currentRealEstatePrice[stringToBytes32(_name)];
+    }
+
+    function realestateArea(string memory _name) private {
+        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
+        request.add("name", _name);
+        request.add("copyPath", "surfaceArea");
+        receiptArea[sendChainlinkRequestTo(oracle, request, fee)] = stringToBytes32(_name);
+    }
+
+    function fulfill(bytes32 _requestId, uint256 _surfaceArea) public recordChainlinkFulfillment(_requestId){
+        tokenHash = receiptArea[_requestId];
+        delete receiptArea[_requestId];
+        currentRealEstateArea[tokenHash] = _surfaceArea;
+        latestRequestId = _requestId;
+        surfaceArea = _surfaceArea;
+    }
+
+    function getCurrentAreaeByAddress(string memory _name) public view returns (uint256) {
+        return currentRealEstateArea[stringToBytes32(_name)];
+    }
+
+    function realestateRent(string memory _name) private {
+        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfillRent.selector);
+        request.add("name", _name);
+        request.add("copyPath", "expectedRent");
+        receiptRent[sendChainlinkRequestTo(oracle, request, fee)] = stringToBytes32(_name);
+    }
+
+    function fulfillRent(bytes32 _requestId, uint256 _rent) public recordChainlinkFulfillment(_requestId){
+        tokenHash = receiptRent[_requestId];
+        delete receiptRent[_requestId];
+        currentRealEstateRent[tokenHash] = _rent;
+        latestRequestId = _requestId;
+        rent = _rent;
+
+    }
+
+    function getCurrentRentByAddress(string memory _name) public view returns (uint256) {
+        return currentRealEstateRent[stringToBytes32(_name)];
+    }
+
+    function stringToBytes32(string memory source) internal pure returns (bytes32 result) {
+        bytes memory tempEmptyStringTest = bytes(source);
+        if (tempEmptyStringTest.length == 0) {
+            return 0x0;
+        }
+        assembly {
+            result := mload(add(source, 32))
+        }
+    }
+
+    function bytes32ToStr(bytes32 _bytes32) private pure returns (string memory) {
+        bytes memory bytesArray = new bytes(32);
+        for (uint256 i; i < 32; i++) {
+            bytesArray[i] = _bytes32[i];
+        }
+        return string(bytesArray);
+    }
+}
---
+++ remix/RealEstateToken.sol
@@ -0,0 +1,118 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.7.0;
+
+//import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+//import "@openzeppelin/contracts/access/Ownable.sol";
+//import "@openzeppelin/contracts/utils/Address.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Address.sol";
+import "./RealestateVerification.sol";
+import "./RealEstateAPIConsumer.sol";
+
+contract RealestateToken is ERC20, Ownable {
+  using SafeMath for uint;
+  using Address  for address;
+
+  event AddShareholder (address indexed owner, address indexed shareholder);
+  event RemoveShareholder (address indexed owner, address indexed shareholder);
+  event TransferRevenue (address indexed sender, address indexed receipent, uint amount);
+
+
+  mapping(address => bool) internal shareholders;
+  mapping (address => uint) internal share;
+  uint public totalShares;
+  uint public collected;
+  uint public propertyArea;
+  uint public propertyPrice;
+  uint public propertyRent;
+  uint public tokenPrice;
+  uint public profitByMonth;
+  //string text;
+
+  RealestateVerification contractInstance = new RealestateVerification();
+  RealEstateAPIConsumer consumer;
+
+  modifier checkShareholder(address _address) {
+    require(isShareHolder(_address), "Not a shareholder");
+   _;
+  }
+
+  constructor(string memory _name) public ERC20("RealEstateToken", "RET"){
+    //require(contractInstance.isVerified(_owner), "property not verified");
+    consumer = new RealEstateAPIConsumer();
+    //consumer.realEstateData(_name);
+    //text = consumer.test();
+    //propertyArea = consumer.getCurrentAreaeByAddress(_name);
+    //propertyRent = consumer.getCurrentRentByAddress(_name);
+    //setTokenPrice(_name);
+    _mint(msg.sender, 1000);
+  }
+
+  function setTokenPrice(string memory _name) private returns(uint){
+    propertyPrice = consumer.getCurrentAreaeByAddress(_name);
+    tokenPrice = propertyArea / propertyPrice;
+    return tokenPrice;
+  }
+
+//   function calculateProfitByMonth(uint _share, uint _propertyRent, uint _propertyArea) public returns(uint){
+//     propertyRent = _propertyRent;
+//     propertyArea = _propertyArea;
+//     share =
+//     profitByMonth = (share * propertyRent) / propertyArea;
+//     return profitByMonth;
+//   }
+
+  function accumulate() external payable {
+    //TODO find a formula to convert amount to shares
+    //This is a dummy
+
+    collected = collected.add(msg.value);
+    //totalShares = collected ;
+    share[_msgSender()] = msg.value;
+  }
+
+  function isShareHolder(address _address) public view returns (bool){
+    return shareholders[_address];
+  }
+
+/**
+ * @dev function to add a shareholder
+ * only allows EOAs to be a shareholder
+ */
+
+  function addShareHolder(address _address) public onlyOwner {
+    require(!_address.isContract(), "Only EOA are allowed as shareholders");
+    require(!isShareHolder(_address), "Already a shareholder");
+    shareholders[_address] = true;
+    emit AddShareholder(owner(), _address);
+  }
+
+  function removeShareHolder(address _address) public onlyOwner checkShareholder(_address) {
+    shareholders[_address] = false;
+    emit RemoveShareholder(owner(), _address);
+
+  }
+
+  //need to figure out, is it to be transfer or distribute
+  function returnRevenue(address receipent, uint amount) internal checkShareholder(receipent) returns (bool) {
+    _transfer(_msgSender(), receipent, amount);
+    emit TransferRevenue(_msgSender(), receipent, amount);
+    return true;
+  }
+
+
+ /* function distributeRevenue(address _address, string memory _name) public checkShareholder(_address){
+    //TODO Formulate appropriate method to distribute the shares. This is a dummy
+
+    // require(share[_address] > 0 , "You don't own any shares");
+    // RealEstateAPIConsumer instance = new  RealEstateAPIConsumer();
+    //
+    // //TODO PLEASE CHECK WHETHER THIS IS THE CORRECT WAY OF RETRIEVING THE CLOSE PRICE !
+    //
+    // instance.realestatePrice(_name);
+    // uint256 closePrice = instance.closePrice();
+    // uint individualProfit = share[_address].div(totalShares).mul(closePrice);
+    // returnRevenue(_address, individualProfit);
+  }*/
+}
